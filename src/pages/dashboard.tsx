988a372707fd55aa11be25d6e660ff711660a10e
Moe Alam <ma3655@rit.edu>
2023-09-23 21:52:37 -0400

    dashboard

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,14 @@
+// components/SignupPage.tsx
+import Link from 'next/link';
+import SignupForm from '../components/SignupForm';
+import MyHeader from '@/components/MyHeader';
+
+const Dashboard = () => {
+    return (
+        <>
+            <MyHeader />
+        </>
+    );
+};
+
+export default Dashboard;
